92ba4f8ddd796345e55a12b7ab3728a62e6a9ab0
aryanpatel2002 <aryanpatel.comp25@gmail.com>
2025-12-17 14:30:04 +0530

    changes

--- FoodXpress/vite.config.js
+++ FoodXpress/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@config': path.resolve(__dirname, './src/config')
+    }
+  }
 })
